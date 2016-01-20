3bd229190bd2bd8cf07ffc18c47984d2beb3f42c
victor <vapinedaus@yahoo.com>
2016-01-20 17:58:53 -0600

    All

---
+++ src/Assignment08/PatternAndMatcherTest.java
@@ -0,0 +1,34 @@
+package Assignment08;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternAndMatcherTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+      System.out.println("========================Plate No===========================");
+      
+	  Pattern licensePattern = Pattern.compile("[A-Z]{2}[0-9]{4}[A-Z]{3}");
+	  Matcher matcher1 = licensePattern.matcher("VA5678CAM");
+      Matcher matcher2 = licensePattern.matcher("XXX5678CA");
+	  System.out.println("Is VA5678CAM a valid plate number: " + matcher1.matches());
+	  System.out.println("Is XXX5678CA a valid plate number: " + matcher2.matches());
+	  
+	  
+	  System.out.println("\n=========================Replace===========================");
+	  Pattern pattern =  Pattern.compile("[8]");
+	  String countStr ="123456789101112131415161718";
+	  Matcher matcher = pattern.matcher(countStr);
+	  System.out.println("Initial value: " + countStr);
+	  System.out.println("Replaced value: " + matcher.replaceAll("eight"));
+
+
+
+	  
+	  
+	  
+	  
+	}
+}
---
+++ src/Assignment08/RegularExpressionTest.java
@@ -0,0 +1,34 @@
+package Assignment08;
+
+public class RegularExpressionTest {
+
+	public RegularExpressionTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+      System.out.println("========================Plate No===========================");
+	  String licensePattern = "[A-Z]{2}[0-9]{4}[A-Z]{3}";
+	  String strPlateNo1= "VA5678CAM";
+	  String strPlateNo2= "XXX5678CA";
+	  System.out.println("Is "+ strPlateNo1 +" a valid plate number: " + strPlateNo1.matches(licensePattern));
+	  System.out.println("Is "+ strPlateNo2 +" a valid plate number: " + strPlateNo2.matches(licensePattern));
+	  
+	  
+	  System.out.println("\n=========================Replace===========================");
+	  String eight = "eight";
+	  String countStr ="123456789101112131415161718";
+	  System.out.println("Initial value: " + countStr);
+	  System.out.println("Replaced value: " + countStr.replaceAll("[8]", eight));
+
+
+
+	  
+	  
+	  
+	  
+	}
+
+}
